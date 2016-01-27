eb2536f2c1705530d23d3ed2d256c87a9eb9db25
Thomas Johansen <thxmasj@gmail.com>
2016-01-27 22:08:11 +0100

    uke 3 - oppgave: ngRouter

--- conFusion/app/scripts/app.js
+++ conFusion/app/scripts/app.js
@@ -1,5 +1,21 @@
 'use strict';
 
-angular.module('confusionApp', [])
+angular.module('confusionApp', ['ngRoute'])
+    .config(function($routeProvider) {
+        $routeProvider
+            .when('/contactus', {
+                templateUrl: 'contactus.html',
+                controller: 'ContactController'
+            })
+            .when('/menu', {
+                templateUrl: 'menu.html',
+                controller: 'MenuController'
+            })
+            .when('/menu/:id', {
+                templateUrl: 'dishdetail.html',
+                controller: 'DishDetailController'
+            })
+            .otherwise('/contactus');
+    })
 
 ;
