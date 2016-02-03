c1bf6aeb655b1bbd385bf6c6cf323b47af35d4a7
Thomas Johansen <thxmasj@gmail.com>
2016-02-03 22:24:22 +0100

    uke 4 - oppgave 2 (Error handling with $resource) {fikset også ødelagt comment-form]

--- conFusion/app/scripts/controllers.js
+++ conFusion/app/scripts/controllers.js
@@ -7,10 +7,18 @@ angular.module('confusionApp')
             $scope.tab = 1;
             $scope.filtText = '';
             $scope.showDetails = false;
-            $scope.showMenu = true;
+            $scope.showMenu = false;
             $scope.message = 'Loading ...';
 
-            $scope.dishes = menuFactory.getDishes().query();
+            $scope.dishes = menuFactory.getDishes().query(
+                function(response) {
+                    $scope.dishes = response;
+                    $scope.showMenu = true;
+                },
+                function(response) {
+                    $scope.message = 'Error: ' + response.status + ' ' + response.statusText;
+                }
+            );
 
             $scope.select = function(setTab) {
                 $scope.tab = setTab;
@@ -70,12 +78,22 @@ angular.module('confusionApp')
         }])
 
         .controller('DishDetailController', ['$scope', '$stateParams', 'menuFactory', function($scope, $stateParams, menuFactory) {
-            $scope.showDish = true;
+            $scope.showDish = false;
             $scope.message = 'Loading ...';
-            $scope.dish = menuFactory.getDishes().get({id: parseInt($stateParams.id,10)});
+            $scope.dish = menuFactory.getDishes()
+                .get({id: parseInt($stateParams.id,10)}).$promise
+                .then(
+                    function(response) {
+                        $scope.dish = response;
+                        $scope.showDish = true;
+                    },
+                    function(response) {
+                        $scope.message = 'Error: ' + response.status + ' ' + response.statusText;
+                    }
+                );
         }])
 
-        .controller('DishCommentController', ['$scope', function($scope) {
+        .controller('DishCommentController', ['$scope', 'menuFactory', function($scope, menuFactory) {
 
             $scope.mycomment = {rating:5, comment:"", author:"", date:""};
 
@@ -85,7 +103,7 @@ angular.module('confusionApp')
                 console.log($scope.mycomment);
 
                 $scope.dish.comments.push($scope.mycomment);
-
+                menuFactory.getDishes().update({id: $scope.dish.id}, $scope.dish);
                 $scope.commentForm.$setPristine();
 
                 $scope.mycomment = {rating:5, comment:"", author:"", date:""};
@@ -95,9 +113,19 @@ angular.module('confusionApp')
         .controller('IndexController', ['$scope', 'corporateFactory', 'menuFactory', function($scope, corporateFactory, menuFactory) {
             $scope.executiveChef = corporateFactory.getLeader(3);
             $scope.promotion = menuFactory.getPromotion(0);
-            $scope.showDish = true;
+            $scope.showDish = false;
             $scope.message = 'Loading ...';
-            $scope.featured = menuFactory.getDishes().get({id: 0});
+            $scope.featured = menuFactory.getDishes()
+                .get({id: 0}).$promise
+                .then(
+                    function(response) {
+                        $scope.featured = response;
+                        $scope.showDish = true;
+                    },
+                    function(response) {
+                        $scope.message = 'Error: ' + response.status + ' ' + response.statusText;
+                    }
+                );
         }])
 
         .controller('AboutController', ['$scope', 'corporateFactory', function($scope, corporateFactory) {
